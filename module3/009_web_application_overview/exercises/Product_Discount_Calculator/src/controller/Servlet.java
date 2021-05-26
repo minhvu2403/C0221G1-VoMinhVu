63588fbc6ae2ba63c833bebb2c9a6e32e12ca575
minhvu2403 <vominhvufc@gmail.com>
2021-05-26 17:27:12 +0700

    26/05/2021

---
+++ module3/009_web_application_overview/exercises/Product_Discount_Calculator/src/controller/Servlet.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Servlet",urlPatterns = "/discount")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        String description = request.getParameter("description");
+        float price = Float.parseFloat(request.getParameter("price"));
+        float percent = Float.parseFloat(request.getParameter("percent"));
+
+        float amount = price * percent * 0.01f;
+        float discountPrice = price - amount;
+
+        writer.println("<html>");
+        writer.println(description);
+        writer.println("<br>Discount Amount = " + amount);
+        writer.println("<br>Discount Price = " + discountPrice);
+        writer.println("</html>");
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
